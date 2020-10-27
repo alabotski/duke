8205c6ab57e0092d28916e374add659521bb1bec
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2020-10-27 15:23:13 +0300

    Duke doing cartwheels https://introcs.cs.princeton.edu/java/15inout/

---
+++ src/main/java/com/duke/Duke.java
@@ -0,0 +1,23 @@
+package com.duke;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+public class Duke {
+
+  public static void main(String[] args) {
+    int images = 17; // number of images
+    int WIDTH = 130, HEIGHT = 80; // images are 130-by-80
+    StdDraw.setCanvasSize(WIDTH, HEIGHT);
+    StdDraw.setXscale(0, WIDTH);
+    StdDraw.setYscale(0, HEIGHT);
+    StdDraw.enableDoubleBuffering();
+    // main animation loop
+    for (int t = 0; true; t++) {
+      int i = 1 + (t % images);
+      String filename = "T" + i + ".gif"; // name of the ith image
+      StdDraw.picture(WIDTH / 2.0, HEIGHT / 2.0, filename);
+      StdDraw.show();
+      StdDraw.pause(100);
+    }
+  }
+}
